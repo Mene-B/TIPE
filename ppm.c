eeda256127c6dce61181ca53a967689653202b4b
Mene-B <82470140+Mene-B@users.noreply.github.com>
2024-04-30 12:47:18 +0200

    Update ppm.c

--- ppm.c
+++ ppm.c
@@ -19,6 +19,58 @@ int read_header (FILE* fichier, int* hauteur, int* largeur){
     return 1;
 }
 
+
+void tree_to_image(dim_tree_t* dim_arbre, char* file_name){
+    FILE* f  = fopen(file_name, "w");
+    int largeur = dim_arbre->largeur;
+    int hauteur = dim_arbre->hauteur;
+    write_header(f, largeur, hauteur);
+
+}
+
+
+void write_header(FILE* f,int largeur, int hauteur ){
+    fprintf(f, "P6");
+    fprintf(f, "\n");
+    char* chiffres = malloc(10*sizeof(char));
+    int i = 0;
+    while(largeur != 0){
+        chiffres[i] = '0'+ largeur%10;
+        largeur = largeur / 10;
+        i++;
+    }
+    while(i > 0){
+        fprintf(f, "%c", chiffres[i-1]);
+        i--;
+    }
+    fprintf(f, "\n");
+    while(hauteur != 0){
+        chiffres[i] = '0'+ hauteur%10;
+        hauteur = hauteur / 10;
+        i++;
+    }
+    while(i > 0){
+        fprintf(f, "%c", chiffres[i-1]);
+        i--;
+    }
+    fprintf(f, "\n");
+    int maxval = 255;
+    while(maxval != 0){
+        chiffres[i] = '0'+ maxval%10;
+        maxval = maxval / 10;
+        i++;
+    }
+    while(i > 0){
+        fprintf(f, "%c", chiffres[i-1]);
+        i--;
+    }
+    fprintf(f, "\n");
+
+}
+
+
+
+
 int main(){
     char* f_n = "sample_1920×1280.ppm";
     FILE* f = fopen(f_n,"r");
