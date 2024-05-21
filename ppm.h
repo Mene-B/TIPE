18d727d0f4cc56135496c0f39a445569a3f6d02a
Mene-B <82470140+Mene-B@users.noreply.github.com>
2024-05-21 12:31:21 +0200

    Update ppm.h

--- ppm.h
+++ ppm.h
@@ -5,13 +5,13 @@ pix_t* read_ppm (char name, int* hauteur, int* largeur, int* taille_max);
 int read_header (FILE* fichier, int* hauteur, int* largeur);
 
 //renvoie la liste des pixels de fichier 
-pix_t* read_body (FILE* fichier, int hauteur, int largeur, int maxval);
-
-//renvoie un entier lu dans fichier 
-int read_int (FILE* fichier);
+pix_t* read_body (FILE* fichier, int hauteur, int largeur, int valmax);
 
 // Créer l'imafe au format ppm à partir de l'arbre avec les données compressées 
-void tree_to_image(dim_tree_t* arbre);
+image_t* tree_to_image(dim_tree_t* arbre);
 
 // Ecrit le header du fichier ppm 
 void write_header(FILE* f, int largeur, int longueur);
+
+// Transforme un arbre en une matrice qui met les pixels aux bons endroits dans la matrice
+void tree_to_matrice(tree_t* arbre, pix_t*** mat, int i, int j, int largeur, int hauteur);;
