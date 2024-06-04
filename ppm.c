7820cba9a26705e37b6c4b4ad890879d8c620656
arthurc31 <95419853+arthurc31@users.noreply.github.com>
2024-06-04 12:11:46 +0200

    Update ppm.c

--- ppm.c
+++ ppm.c
@@ -1,12 +1,13 @@
 #include "ppm.h"
+#include "quadtree.h"
+#include <assert.h>
 #include <stdio.h>
 #include <assert.h>
 
-int read_header (FILE* fichier, int* hauteur, int* largeur){
+void read_header (FILE* fichier, int* hauteur, int* largeur){
     assert(fichier != NULL);
     char* s = malloc(3*sizeof(char));
     fscanf(fichier, "%s", s);
-    printf("%s\n", s);
     char poubelle;
     char c = '0';
     int width=0;
@@ -33,7 +34,6 @@ int read_header (FILE* fichier, int* hauteur, int* largeur){
     // printf("hauteur : %d\n", height);
     // printf("largeur : %d\n", width);
     // printf("maxval : %d\n", maxval);
-    return maxval;
 }
 
 int read_int(FILE* fichier){
@@ -60,16 +60,15 @@ pix_t*** read_body (FILE* fichier, int hauteur, int largeur){
     return res;
 }
 
-image_t* read_ppm (char* name, int* p_hauteur, int* p_largeur, int* p_taille_max){
+image_t* read_ppm (char* name, int* p_hauteur, int* p_largeur){
     FILE* f = fopen(name, "r");
-    int maxval = read_header (f, p_hauteur, p_largeur);
+    read_header (f, p_hauteur, p_largeur);
     image_t* res = malloc(sizeof(image_t));
     res -> hauteur = *p_hauteur;
     res -> largeur = *p_largeur;
     res -> pixels = read_body(f,*p_hauteur,*p_largeur);
     return res;
 }
-
 
 void tree_to_matrice(tree_t* arbre, pix_t*** mat, int i, int j, int largeur, int hauteur){
     int new_largeur = (int) (largeur/2);
@@ -78,7 +77,7 @@ void tree_to_matrice(tree_t* arbre, pix_t*** mat, int i, int j, int largeur, int
         tree_to_matrice(arbre->enfants[0], mat, i, j, new_largeur, new_hauteur);
         tree_to_matrice(arbre->enfants[1], mat, i, j + new_largeur, largeur - new_largeur, hauteur);
         tree_to_matrice(arbre->enfants[2], mat, i + new_hauteur, j, new_largeur, hauteur -new_hauteur);
-        tree_to_matrice(arbre->enfants[3], mat, i + new_largeur, j + new_hauteur, largeur -  new_largeur, hauteur -  new_hauteur);
+        tree_to_matrice(arbre->enfants[3], mat, i + new_hauteur, j + new_largeur, largeur -  new_largeur, hauteur -  new_hauteur);
     }else {
         for (int k = i; k < i + hauteur; k++){
             for (int p = j; p < j + largeur; p++){
@@ -96,13 +95,14 @@ void tree_to_matrice(tree_t* arbre, pix_t*** mat, int i, int j, int largeur, int
 image_t* tree_to_image(dim_tree_t* dim_arbre){
     int largeur = dim_arbre->largeur;
     int hauteur = dim_arbre->hauteur;
+
     tree_t* arbre = dim_arbre->arbre;
     pix_t*** mat = malloc(hauteur*sizeof(pix_t**));
     for(int i = 0; i < hauteur ; i++){
         mat[i] = malloc(largeur*sizeof(pix_t*));
     }
     tree_to_matrice(arbre, mat, 0, 0, largeur, hauteur);
-    image_t* image = malloc(sizeof(image));
+    image_t* image = malloc(sizeof(image_t));
     image->hauteur = hauteur;
     image->largeur = largeur;
     image->pixels = mat;
@@ -110,17 +110,9 @@ image_t* tree_to_image(dim_tree_t* dim_arbre){
 }
 
 
-void img_to_ppm (char* filename, image_t* image){
-    FILE* f  = fopen(filename, "w");
-    int largeur = image->largeur;
-    int hauteur = image->hauteur;
-    write_header(f, image->largeur, image->hauteur);
-    write_body (f, image->largeur, image->hauteur, image->pixels);
-}
-
 void write_header(FILE* f,int largeur, int hauteur){
-    fprintf(f, "P6");
-    fprintf(f, "\n");
+    assert(f!=NULL);
+    fprintf(f, "P6\n");
     char* chiffres = malloc(10*sizeof(char));
     int i = 0;
     while(largeur != 0){
@@ -165,4 +157,14 @@ void write_body(FILE* f, int largeur, int hauteur, pix_t*** pixels){
             fprintf(f, "%c", pixels[i][j]->b);
         }
     }
+}
+
+void img_to_ppm (char* filename, image_t* image){
+    FILE* f  = fopen(filename, "w");
+    int largeur = image->largeur;
+    int hauteur = image->hauteur;
+    printf("%s\n", filename);
+    write_header(f, largeur, hauteur);
+    write_body (f, largeur, hauteur, image->pixels);
+    fclose(f);
 }
