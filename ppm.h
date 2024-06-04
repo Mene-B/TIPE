49164a77f3a7bea0d6ca7f7bb2ff51e5cfb23ef3
Mene-B <82470140+Mene-B@users.noreply.github.com>
2024-06-04 11:31:22 +0200

    Update ppm.h
    modif dans le .h

--- ppm.h
+++ ppm.h
@@ -1,3 +1,5 @@
+#include "quadtree.h"
+
 //renvoie la liste des pixels du fichier de nom name et stocke la hauteur, largeur et taille maximum. 
 pix_t* read_ppm (char name, int* hauteur, int* largeur, int* taille_max);
 
@@ -5,7 +7,7 @@ pix_t* read_ppm (char name, int* hauteur, int* largeur, int* taille_max);
 int read_header (FILE* fichier, int* hauteur, int* largeur);
 
 //renvoie la liste des pixels de fichier 
-pix_t* read_body (FILE* fichier, int hauteur, int largeur, int valmax);
+pix_t*** read_body (FILE* fichier, int hauteur, int largeur, int valmax);
 
 // Créer l'imafe au format ppm à partir de l'arbre avec les données compressées 
 image_t* tree_to_image(dim_tree_t* arbre);
